ddff106c2a84678e298735616a02fe27af39477e
NEIL MARCOS PERES <neilmarcos2018@gmail.com>
2025-08-31 03:24:39 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,17 @@
-const CACHE = 'diskmsg-v11'; self.addEventListener('fetch', e=>{});
+const CACHE = 'diskmsg-v11';
+const PRECACHE = [
+  './','./index.html','./manifest.json',
+  './assets/icon-192.png','./assets/icon-512.png','./assets/logo.png',
+  './catalog.json'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(PRECACHE)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
