c1e3bfd29b1d8d78dfed425c57684129cff49f4b
NEIL MARCOS PERES <neilmarcos2018@gmail.com>
2025-08-31 02:38:33 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'diskmsg-v9';
+const CACHE = 'diskmsg-v10';
 const PRECACHE = [
   './',
   './index.html',
@@ -8,21 +8,14 @@ const PRECACHE = [
   './assets/logo.png',
   './catalog.json',
 ];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(PRECACHE)));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(PRECACHE)));
   self.skipWaiting();
 });
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    )
-  );
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k!==CACHE).map(k => caches.delete(k)))));
   self.clients.claim();
 });
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(caches.match(event.request).then((resp) => resp || fetch(event.request)));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
