4606b8449374106054be2d0e69a008d7f2a07d2e
NEIL MARCOS PERES <neilmarcos2018@gmail.com>
2025-08-31 01:52:12 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'diskmsg-v9';
+const CACHE = 'diskmsg-v5';
 const PRECACHE = [
   './',
   './index.html',
@@ -9,20 +9,15 @@ const PRECACHE = [
   './catalog.json',
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(PRECACHE)));
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(PRECACHE)));
   self.skipWaiting();
 });
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
 });
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(caches.match(event.request).then((resp) => resp || fetch(event.request)));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
 });
